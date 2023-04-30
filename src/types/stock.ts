6a7f213f046142a3b608e931908cee718b8b1c81
FitiavanaHerinarivo <130699983+FitiavanaHerinarivo@users.noreply.github.com>
2023-04-30 19:28:31 +0300

    fix indentation

--- src/types/stock.ts
+++ src/types/stock.ts
@@ -1,36 +1,34 @@
 export type tDataStocks = {
-    id: number;
-    barrcode: string;
-    name: string;
-    cost: number;
-    price: number;
-    quantity: number;
-    type:'FULL_UNIT' | 'CONSUMABLE' | 'PARTIAL_UNIT' | 'PACKS';
+  id: number;
+  barrcode: string;
+  name: string;
+  cost: number;
+  price: number;
+  quantity: number;
+  type: 'FULL_UNIT' | 'CONSUMABLE' | 'PARTIAL_UNIT' | 'PACKS';
 };
 export type TMappingStockType = {
-    [type in 'FULL_UNIT' | 'CONSUMABLE' | 'PARTIAL_UNIT' | 'PACKS']: {
-        text: string;
-        color: string;
-    };
-   
+  [type in 'FULL_UNIT' | 'CONSUMABLE' | 'PARTIAL_UNIT' | 'PACKS']: {
+    text: string;
+    color: string;
+  };
+
 };
-export const mappingStockType : TMappingStockType  = {
-    FULL_UNIT: {
-      text: 'Full Unit',
-      color: 'text-green-400'
-    },
-    CONSUMABLE: {
+export const mappingStockType: TMappingStockType = {
+  FULL_UNIT: {
+    text: 'Full Unit',
+    color: 'text-green-400'
+  },
+  CONSUMABLE: {
     text: 'Consumable',
     color: 'text-blue-400'
   },
-    PARTIAL_UNIT: {
-        text: 'Partial Unit',
-        color: 'text-white'
-      },
-
-
-    PACKS: {
-        text: 'Packs',
-        color: 'text-red-500'
-      },
+  PARTIAL_UNIT: {
+    text: 'Partial Unit',
+    color: 'text-white'
+  },
+  PACKS: {
+    text: 'Packs',
+    color: 'text-red-500'
+  },
 };
