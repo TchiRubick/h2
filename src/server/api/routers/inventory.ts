4fa13c34c7dfd3066a2d36bc9149e4967944b088
anderson suiriex <andersonsuiriex@gmail.com>
2023-05-04 23:16:18 +1300

    connect table

--- src/server/api/routers/inventory.ts
+++ src/server/api/routers/inventory.ts
@@ -1,10 +1,14 @@
-
+import { TypesProduct } from "@prisma/client";
+import { z } from "zod";
 import getInventories from "~/services/inventory/getInventories";
 import { createTRPCRouter,publicProcedure } from "../trpc";
 
 export const inventoryRouter = createTRPCRouter (
     {
         getList: publicProcedure
+        .input(z.object({ barcode: z.string(), name: z.string(), cost: z.number().optional(),
+              price: z.number().optional(), quantity: z.number().optional(),
+               type: z.nativeEnum(TypesProduct) }))
         .query(()=> getInventories),
     }
 )
