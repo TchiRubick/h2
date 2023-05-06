87cf727d4d7925ab5b2f394d7e61caa2325f2252
Tchi <tchi.devica@gmail.com>
2023-05-06 19:57:30 +0300

    fix: build

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,4 @@
 import { createTRPCRouter } from "~/server/api/trpc";
-import { exampleRouter } from "~/server/api/routers/example";
 import {inventoryRouter} from "~/server/api/routers/inventory";
 
 /**
@@ -8,7 +7,6 @@ import {inventoryRouter} from "~/server/api/routers/inventory";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
   inventory: inventoryRouter,
 });
 
